67d3517568d20a0f1be8b155ff00ae0592d51117
Amin Bandali <me@aminb.org>
2015-03-28 20:16:34 -0400

    lab l - add LabL9 and missing LabL8 source blocks

---
+++ l/yAlu.v
@@ -0,0 +1,20 @@
+module yAlu (z, ex, a, b, op) ;
+
+   input [31:0] a, b;
+   input [2:0]  op;
+   output [31:0] z;
+   output        ex;
+   wire          cout;
+   wire [31:0]   alu_and, alu_or, alu_arith, slt;
+
+   // not supported
+   assign slt = 0;
+   assign ex = 0;
+
+   // instantiate the components and connect them
+   and m_and [31:0] (alu_and, a, b);
+   or m_or [31:0] (alu_or, a, b);
+   yArith m_arith(alu_arith, cout, a, b, op[2]);
+   yMux4to1 #(.SIZE(32)) mux(z, alu_and, alu_or, alu_arith, slt, op[1:0]);
+
+endmodule // yAlu
